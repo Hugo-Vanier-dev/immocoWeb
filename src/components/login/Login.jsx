c35c8f55bdaf680b4fc6a72b07448c1b4a833e27
GauT.C <gautier.chassang@gmail.com>
2021-03-05 09:32:32 +0100

    Ajout du "form" sur login

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -10,10 +10,12 @@ function LoginPage() {
                     <img src={logo} alt="logo_ImmoCo" className="LogoIcoPicture" />
                 </div>
                 <div className=" grid grid-rows-4">
-                    <a className="uppercase text-center align-middle text-blue-800 underline text-opacity-75 font-bold text-2xl font-sans">immoco</a>
-                    <input type="text" className="LoginMail grid row-start-2 border-2 border-white bg-gray-300 m-2 p-2 rounded-md text-center" placeholder="@" />
-                    <input type="text" className="LoginPassword grid row-start-3 border-2 border-white bg-gray-300 m-2 p-2 rounded-md text-center" placeholder="mot de passe" />
-                    <input type="button" value="Connexion" className="LoginPageButton text-green-200 uppercase w-1/2 grid row-start-4 auto-cols-auto font-bold  p-2 pt-2 pb-2 rounded-2xl bg-green-400 border-2 border-green-200 shadow " />
+                    <form action="POST">
+                        <a className="uppercase text-center align-middle text-blue-800 underline text-opacity-75 font-bold text-2xl font-sans">immoco</a>
+                        <input type="text" className="LoginMail grid  row-start-2 border-2 border-white bg-gray-300 m-2 p-2 rounded-md text-center" placeholder="@" />
+                        <input type="text" className="LoginPassword grid  row-start-3 border-2 border-white bg-gray-300 m-2 p-2 rounded-md text-center" placeholder="mot de passe" />
+                        <input type="button" value="Connexion" className="LoginPageButton text-green-200 uppercase w-1/2 grid row-start-4 auto-cols-auto font-bold  p-2 pt-2 pb-2 rounded-2xl bg-green-400 border-2 border-green-200 shadow " />
+                    </form>
                 </div>
             </div>
         </div>
