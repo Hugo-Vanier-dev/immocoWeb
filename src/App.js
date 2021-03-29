d6607ffe78add9f89846a0044065149a8f34c3e0
Hugo-Vanier-dev <55690800+Hugo-Vanier-dev@users.noreply.github.com>
2021-03-29 12:09:20 +0200

    ajout de la sidebar dans le router

--- src/App.js
+++ src/App.js
@@ -1,62 +1,73 @@
-import React from 'react';
-import './App.css';
-// import Navbar from './components/navigation/Navbar';
+import React from "react";
+import "./App.css";
+import SideMenu from "./components/navigation/sideMenu/SideMenu";
 
-import CreateClient from './components/clients/CreateClient';
-import ReadClient from './components/clients/ReadClient';
-import UpdateClient from './components/clients/UpdateClient';
-import DeleteClient from './components/clients/DeleteClient';
+import CreateClient from "./components/clients/CreateClient";
+import ReadClient from "./components/clients/ReadClient";
+import UpdateClient from "./components/clients/UpdateClient";
+import DeleteClient from "./components/clients/DeleteClient";
 
-import CreateRdv from './components/planning/CreateRdv';
-import ReadRdv from './components/planning/ReadRdv';
-import UpdateRdv from './components/planning/UpdateRdv';
-import DeleteRdv from './components/planning/DeleteRdv';
+import CreateRdv from "./components/planning/CreateRdv";
+import ReadRdv from "./components/planning/ReadRdv";
+import UpdateRdv from "./components/planning/UpdateRdv";
+import DeleteRdv from "./components/planning/DeleteRdv";
 
-import CreateProperty from './components/properties/CreateProperty';
-import ReadProperty from './components/properties/ReadProperty';
-import UpdateProperty from './components/properties/UpdateProperty';
-import DeleteProperty from './components/properties/DeleteProperty';
+import CreateProperty from "./components/properties/CreateProperty";
+import ReadProperty from "./components/properties/ReadProperty";
+import UpdateProperty from "./components/properties/UpdateProperty";
+import DeleteProperty from "./components/properties/DeleteProperty";
 
-import CreateUser from './components/users/CreateUser';
-import ReadUser from './components/users/ReadUser';
-import UpdateUser from './components/users/UpdateUser';
-import DeleteUser from './components/users/DeleteUser';
+import CreateUser from "./components/users/CreateUser";
+import ReadUser from "./components/users/ReadUser";
+import UpdateUser from "./components/users/UpdateUser";
+import DeleteUser from "./components/users/DeleteUser";
 
-import Login from './components/login/Login';
+import LoginPage from "./components/login/Login";
 
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+function Home() {
+  return (
+    <div>
+      <h1>Home Page</h1>
+    </div>
+  );
+}
 
 function App() {
-    return (
-        <Router>
-            <switch>
-                
-            <Route path="/CreateClient" component={CreateClient}/>
-            <Route path="/UpdateClient" component={UpdateClient}/>
-            <Route path="/ReadClient" component={ReadClient}/>
-            <Route path="/DeleteClient" component={DeleteClient}/>
-            
-            <Route path="/CreateRdv" component={CreateRdv}/>
-            <Route path="/UpdateRdv" component={UpdateRdv}/>
-            <Route path="/ReadRdv" component={ReadRdv}/>
-            <Route path="/DeleteRdv" component={DeleteRdv}/>
-            
-            <Route path="/CreateProperty" component={CreateProperty}/>
-            <Route path="/UpdateProperty" component={UpdateProperty}/>
-            <Route path="/ReadProperty" component={ReadProperty}/>
-            <Route path="/DeleteProperty" component={DeleteProperty}/>
-            
-            <Route path="/CreateUser" component={CreateUser}/>
-            <Route path="/UpdateUser" component={UpdateUser}/>
-            <Route path="/ReadUser" component={ReadUser}/>
-            <Route path="/DeleteUser" component={DeleteUser}/>
-            
+  return (
+    <div className="App">
+      <Router>
+          <div className= "align-component">
+          <SideMenu />
+            <Switch>
+              <Route path="/" exact component={LoginPage} />
+              <Route path="/home" component={Home} />
 
-            <Route path="/login" component={Login}/>
+              <Route path="/createClient" component={CreateClient} />
+              <Route path="/updateClient" component={UpdateClient} />
+              <Route path="/readClient" component={ReadClient} />
+              <Route path="/deleteClient" component={DeleteClient} />
 
-            </switch>
-        </Router>
-    );
+              <Route path="/createRdv" component={CreateRdv} />
+              <Route path="/updateRdv" component={UpdateRdv} />
+              <Route path="/readRdv" component={ReadRdv} />
+              <Route path="/deleteRdv" component={DeleteRdv} />
+
+              <Route path="/createProperty" component={CreateProperty} />
+              <Route path="/updateProperty" component={UpdateProperty} />
+              <Route path="/readProperty" component={ReadProperty} />
+              <Route path="/deleteProperty" component={DeleteProperty} />
+
+              <Route path="/createUser" component={CreateUser} />
+              <Route path="/updateUser" component={UpdateUser} />
+              <Route path="/readUser" component={ReadUser} />
+              <Route path="/deleteUser" component={DeleteUser} />
+            </Switch>
+            </div>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
