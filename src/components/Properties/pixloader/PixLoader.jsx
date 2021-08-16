9c85a50021952bbed029a4c9c608a36252033dcd
GauT.C <gautier.chassang@gmail.com>
2021-08-16 17:28:26 +0200

    Dynamisation de la galerie et mise en forme du template.

--- src/components/Properties/pixloader/PixLoader.jsx
+++ src/components/Properties/pixloader/PixLoader.jsx
@@ -1,58 +1,14 @@
 import React from "react";
-import Gallery from "react-photo-gallery";
+import "./PixLoader.css";
 
-import  "./PixLoader.css";
-
-const Photos = [
-    {
-        src: "/upload/test_00001.jpg",
-        width: 3,
-        height: 4
-      },
-      {
-          src: "/upload/test_00002.jpg",
-          width: 3,
-          height: 4
-        },
-        {
-            src: "/upload/test_00003.jpg",
-            width: 3,
-            height: 4
-          },
-          {
-              src: "/upload/test_00004.jpg",
-              width: 3,
-              height: 4
-            },
-            {
-                src: "/upload/test_00005.jpg",
-                width: 3,
-                height: 4
-              },
-              {
-                  src: "/upload/test_00006.jpg",
-                  width: 3,
-                  height: 4
-                },
-                {
-                    src: "/upload/test_00007.jpg",
-                    width: 3,
-                    height: 4
-                  },
-                  {
-                      src: "/upload/test_00008.jpg",
-                      width: 3,
-                      height: 4
-                    },
-];
+const pixCount = 5;
+/** src: "/upload/test_00001.jpg" */
 
 function PixLoader() {
-
+  const index = 1;
   return (
     <div className="py-2 mx-2 text-gray-300">
-      <div className="text-blue-400 font-extrabold">
-        Galerie photo
-      </div>
+      <div className="text-blue-400 font-extrabold">Galerie photo</div>
       <div>
         <input
           className="m-4 bg-blue-300 text-center text-gray-50 p-2 rounded-lg"
@@ -62,8 +18,16 @@ function PixLoader() {
           id=""
         />
       </div>
-      <div id="pixGallery" className="">
-        <Gallery className="galleryComponents" photos={Photos} direction={"row"} />
+      <div id="pixGallery">
+        {Array(pixCount)
+          .fill(1)
+          .map((value, index) => (
+            <div key={index} id="pixContainer" className={`picture_${index+1} ${index+1 == pixCount ? 'flex-grow' : ''}`}>
+              <a href="#">
+                <img className={`h-full w-full object-cover`} src={`/upload/test_0000${index + 1}.jpg`} alt="" />{" "}
+              </a>
+            </div>
+          ))}
       </div>
     </div>
   );
