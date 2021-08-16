20ea8b49c8c11cd3a2a6d787af3707ad0dbb081a
GauT.C <gautier.chassang@gmail.com>
2021-08-16 11:39:17 +0200

    Le dossier d'images est désormais dans le dossier public. Création d'un fichier css pour les images à charger. Réfection de la gallerie.

--- src/components/Properties/pixloader/PixLoader.jsx
+++ src/components/Properties/pixloader/PixLoader.jsx
@@ -1,87 +1,49 @@
 import React from "react";
 import Gallery from "react-photo-gallery";
 
+import  "./PixLoader.css";
+
 const Photos = [
     {
-        src: "https://source.unsplash.com/2ShvY8Lf6l0/800x599",
-        width: 4,
-        height: 3
-      },
-      {
-        src: "https://source.unsplash.com/Dm-qxdynoEc/800x799",
-        width: 1,
-        height: 1
-      },
-      {
-        src: "https://source.unsplash.com/qDkso9nvCg0/600x799",
+        src: "/upload/test_00001.jpg",
         width: 3,
         height: 4
       },
       {
-        src: "https://source.unsplash.com/iecJiKe_RNg/600x799",
-        width: 3,
-        height: 4
-      },
-      {
-        src: "https://source.unsplash.com/epcsn8Ed8kY/600x799",
-        width: 3,
-        height: 4
-      },
-      {
-        src: "https://source.unsplash.com/NQSWvyVRIJk/800x599",
-        width: 4,
-        height: 3
-      },
-      {
-        src: "https://source.unsplash.com/zh7GEuORbUw/600x799",
-        width: 3,
-        height: 4
-      },
-      {
-        src: "https://source.unsplash.com/PpOHJezOalU/800x599",
-        width: 4,
-        height: 3
-      },
-      {
-        src: "https://source.unsplash.com/I1ASdgphUH4/800x599",
-        width: 4,
-        height: 3
-      },
-      {
-        src: "https://source.unsplash.com/XiDA78wAZVw/600x799",
-        width: 3,
-        height: 4
-      },
-      {
-        src: "https://source.unsplash.com/x8xJpClTvR0/800x599",
-        width: 4,
-        height: 3
-      },
-      {
-        src: "https://source.unsplash.com/qGQNmBE7mYw/800x599",
-        width: 4,
-        height: 3
-      },
-      {
-        src: "https://source.unsplash.com/NuO6iTBkHxE/800x599",
-        width: 4,
-        height: 3
-      },
-      {
-        src: "https://source.unsplash.com/pF1ug8ysTtY/600x400",
-        width: 4,
-        height: 3
-      },
-      {
-        src: "https://source.unsplash.com/A-fubu9QJxE/800x533",
-        width: 4,
-        height: 3
-      },
-      {
-        src: "https://source.unsplash.com/5P91SF0zNsI/740x494",
-        width: 4,
-        height: 3
-      }
+          src: "/upload/test_00002.jpg",
+          width: 3,
+          height: 4
+        },
+        {
+            src: "/upload/test_00003.jpg",
+            width: 3,
+            height: 4
+          },
+          {
+              src: "/upload/test_00004.jpg",
+              width: 3,
+              height: 4
+            },
+            {
+                src: "/upload/test_00005.jpg",
+                width: 3,
+                height: 4
+              },
+              {
+                  src: "/upload/test_00006.jpg",
+                  width: 3,
+                  height: 4
+                },
+                {
+                    src: "/upload/test_00007.jpg",
+                    width: 3,
+                    height: 4
+                  },
+                  {
+                      src: "/upload/test_00008.jpg",
+                      width: 3,
+                      height: 4
+                    },
 ];
 
 function PixLoader() {
@@ -100,8 +62,8 @@ function PixLoader() {
           id=""
         />
       </div>
-      <div id="pixGallery" className="w-full">
-        <Gallery photos={Photos} direction={"row"} />
+      <div id="pixGallery" className="">
+        <Gallery className="galleryComponents" photos={Photos} direction={"row"} />
       </div>
     </div>
   );
