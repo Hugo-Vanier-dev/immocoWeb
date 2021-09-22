f490083f6d211159699e74d205350f34184fda7a
Hugo <hugovanierdesaintaunay@gmail.com>
2021-09-22 15:23:58 +0200

    fast push

--- src/components/Clients/ClientPage.jsx
+++ src/components/Clients/ClientPage.jsx
@@ -19,19 +19,7 @@ setData(res.data);
 return(
 <div className="grid grid-cols-3">
   <div className="col-span-1 col-start-1">
-    <NewClientForm edit={true}/>
-      <div>
-        <input
-          type="submit"
-          value="Créer"
-          className="m-auto mt-5 text-white uppercase bg-green-300 hover:bg-green-600 font-bold p-2 pt-2 pb-2 mx-2 rounded-xl"
-        />
-        <input
-          type="button"
-          value="Annuler"
-          className="m-auto mt-5 text-white uppercase bg-red-300 hover:bg-red-600 font-bold p-2 pt-2 pb-2 mx-2 rounded-xl"
-        />
-      </div>
+    <ClientForm edit={true}/>
   </div>
   <div className="col-span-1 col-start-2">
     <ClientList edit={true} setClientId={setClientId}/>
--- src/components/Clients/form/ClientForm.jsx
+++ src/components/Clients/form/ClientForm.jsx
@@ -14,18 +14,12 @@ function ClientForm({ clientId = null, edit = false }) {
   const mailregex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   const telRegex = /^\d{2}(\s\d{2}){4}$/;
 
-  const [redirectClientListe, setRedirectClientListe] = React.useState(false);
-  const [redirectInfoClient, setredirectInfoClient] = React.useState(false);
-
   const currentUser = UseUserContext();
   const [users, setUsers] = React.useState(null);
   const [clientTypes, setClientTypes] = React.useState(null);
 
   const [modeEdit, setModeEdit] = React.useState(edit);
 
-
-
-console.log(clientId);
 
   const [formErrors, setFormErrors] = React.useState({
     firstname: null,
@@ -69,7 +63,7 @@ console.log(clientId);
       const data = formValues;
       if (clientId) {
         ClientService.update(clientId, data).then((res) => {
-          modeEdit = false;
+          setModeEdit(false);
           toast.info("Le client a bien été modifié.", {
             position: "bottom-center",
             autoClose: 5000,
@@ -78,10 +72,8 @@ console.log(clientId);
             draggable: true,
             hideProgressBar: false,
           });
-          setredirectInfoClient(true);
         });
       } else {
-        console.log('coucou');
         ClientService.create(data).then((res) => {
           toast.info("Le client a bien été créé.", {
             position: "bottom-center",
@@ -90,8 +82,7 @@ console.log(clientId);
             pauseOnHover: true,
             draggable: true,
             hideProgressBar: false,
-          });
-          setRedirectClientListe(true);
+          })
         });
       }
     }
@@ -173,16 +164,10 @@ console.log(clientId);
     clientId
   ]);
 
-  if (redirectClientListe) {
-    return <Redirect to="/home" />;
-  }
-  if (redirectInfoClient) {
-    return <Redirect to={`/readClient/${clientId}`} />
-  }
   return (
     <div className="">
       <div className="">
-      <div className="py-2 mx-2 text-blue-300 font-black">Informations clients</div>
+        <div className="py-2 mx-2 text-blue-300 font-black">Informations clients</div>
 
         <div className="grid grid-rows-1">
           <form onSubmit={(e) => submitForm(e)} className="grid grid-cols-1">
@@ -208,111 +193,111 @@ console.log(clientId);
                 readOnly={!modeEdit}
                 className="col-start-1 m-2 p-2 rounded-md text-center border-2 border-blue-300"
                 placeholder="Prénom"
-                />
+              />
               {formErrors.firstname && <p>{formErrors.firstname}</p>}
             </div>
 
             <div className="grid grid-cols-8 justify-items-stretch w-auto">
               <div className="col-span-2">Adresse mail</div>
-                <input
-                  type="email"
-                  name="mail"
-                  onChange={modeEdit ? (e) => mailChange(e) : null}
-                  value={formValues.mail}
-                  readOnly={!modeEdit}
-                  className=" col-start-3 col-span-6 bg-white m-2 p-2 rounded-md text-center border-2 border-blue-300"
-                  placeholder="@"
-                />
+              <input
+                type="email"
+                name="mail"
+                onChange={modeEdit ? (e) => mailChange(e) : null}
+                value={formValues.mail}
+                readOnly={!modeEdit}
+                className=" col-start-3 col-span-6 bg-white m-2 p-2 rounded-md text-center border-2 border-blue-300"
+                placeholder="@"
+              />
               {formErrors.mail && <p>{formErrors.mail}</p>}
             </div>
 
             <div className="flex justify-end">
               <div>Tél.</div>
-                <input
-                  type="text"
-                  name="phone"
-                  onChange={modeEdit ? (e) => telChange(e) : null}
-                  value={formValues.phone}
-                  readOnly={!modeEdit}
-                  className="col-start-1 bg-white m-2 p-2 rounded-md text-center border-2 border-blue-300"
-                  placeholder="03 xx xx xx xx"
-                />
+              <input
+                type="text"
+                name="phone"
+                onChange={modeEdit ? (e) => telChange(e) : null}
+                value={formValues.phone}
+                readOnly={!modeEdit}
+                className="col-start-1 bg-white m-2 p-2 rounded-md text-center border-2 border-blue-300"
+                placeholder="03 xx xx xx xx"
+              />
               {formErrors.phone && <p>{formErrors.phone}</p>}
               <div>Port.</div>
-                <input
-                  type="text"
-                  name="cellphone"
-                  onChange={modeEdit ? (e) => telChange(e) : null}
-                  value={formValues.cellphone}
-                  readOnly={!modeEdit}
-                  className="col-start-2 bg-white m-2 p-2 rounded-md text-center border-2 border-blue-300"
-                  placeholder="06 xx xx xx xx"
-                />
+              <input
+                type="text"
+                name="cellphone"
+                onChange={modeEdit ? (e) => telChange(e) : null}
+                value={formValues.cellphone}
+                readOnly={!modeEdit}
+                className="col-start-2 bg-white m-2 p-2 rounded-md text-center border-2 border-blue-300"
+                placeholder="06 xx xx xx xx"
+              />
               {formErrors.cellphone && <p>{formErrors.cellphone}</p>}
             </div>
 
 
-                <div className="flex justify-center">Adresse postale</div>
-                <div className="flex justify-end">
-                  <div>N°</div>
-                    <input
-                      type="text"
-                      name="streetNumber"
-                      onChange={modeEdit ? (e) => handleChange(e) : null}
-                      value={formValues.streetNumber}
-                      readOnly={!modeEdit}
-                      className="bg-white m-2 p-2 rounded-md text-left border-2 border-blue-300"
-                      placeholder="N°"
-                    />
-                  {formErrors.streetNumber && <p>{formErrors.streetNumber}</p>}
-                  <div>Voie</div>
-                    <input
-                      type="text"
-                      name="streetName"
-                      onChange={modeEdit ? (e) => handleChange(e) : null}
-                      value={formValues.streetName}
-                      readOnly={!modeEdit}
-                      className="bg-white m-2 p-2 rounded-md text-left border-2 border-blue-300"
-                      placeholder="Rue"
-                    />
-                  {formErrors.streetName && <p>{formErrors.streetName}</p>}
-                </div>
-              <div className="flex justify-end">
-                <div>Ville</div>
-                  <input
-                    type="text"
-                    name="city"
-                    onChange={modeEdit ? (e) => handleChange(e) : null}
-                    value={formValues.city}
-                    readOnly={!modeEdit}
-                    className="bg-white m-2 p-2 rounded-md text-left border-2 border-blue-300"
-                    placeholder="Ville"
-                  />
-                {formErrors.city && <p>{formErrors.city}</p>}
-                <div>Code postal</div>
-                  <input
-                    type="text"
-                    name="zipCode"
-                    onChange={modeEdit ? (e) => handleChange(e) : null}
-                    value={formValues.zipCode}
-                    readOnly={!modeEdit}
-                    className="bg-white m-2 p-2 rounded-md text-left border-2 border-blue-300"
-                    placeholder="CP"
-                  />
-                </div>
-              {formErrors.zipCode && <p>{formErrors.zipCode}</p>}
-              
+            <div className="flex justify-center">Adresse postale</div>
+            <div className="flex justify-end">
+              <div>N°</div>
+              <input
+                type="text"
+                name="streetNumber"
+                onChange={modeEdit ? (e) => handleChange(e) : null}
+                value={formValues.streetNumber}
+                readOnly={!modeEdit}
+                className="bg-white m-2 p-2 rounded-md text-left border-2 border-blue-300"
+                placeholder="N°"
+              />
+              {formErrors.streetNumber && <p>{formErrors.streetNumber}</p>}
+              <div>Voie</div>
+              <input
+                type="text"
+                name="streetName"
+                onChange={modeEdit ? (e) => handleChange(e) : null}
+                value={formValues.streetName}
+                readOnly={!modeEdit}
+                className="bg-white m-2 p-2 rounded-md text-left border-2 border-blue-300"
+                placeholder="Rue"
+              />
+              {formErrors.streetName && <p>{formErrors.streetName}</p>}
+            </div>
+            <div className="flex justify-end">
+              <div>Ville</div>
+              <input
+                type="text"
+                name="city"
+                onChange={modeEdit ? (e) => handleChange(e) : null}
+                value={formValues.city}
+                readOnly={!modeEdit}
+                className="bg-white m-2 p-2 rounded-md text-left border-2 border-blue-300"
+                placeholder="Ville"
+              />
+              {formErrors.city && <p>{formErrors.city}</p>}
+              <div>Code postal</div>
+              <input
+                type="text"
+                name="zipCode"
+                onChange={modeEdit ? (e) => handleChange(e) : null}
+                value={formValues.zipCode}
+                readOnly={!modeEdit}
+                className="bg-white m-2 p-2 rounded-md text-left border-2 border-blue-300"
+                placeholder="CP"
+              />
+            </div>
+            {formErrors.zipCode && <p>{formErrors.zipCode}</p>}
+
             <div className="grid grid-cols-1 gap-4">
               <div className="flex justify-center">Informations complémentaires</div>
-                <textarea
-                  type="text"
-                  name="description"
-                  onChange={modeEdit ? (e) => handleChange(e) : null}
-                  value={formValues.description}
-                  readOnly={!modeEdit}
-                  className="infoArea bg-white m-2 p-2 rounded-md text-start border-2 border-blue-300"
-                  placeholder="Informations complémentaires"
-                />
+              <textarea
+                type="text"
+                name="description"
+                onChange={modeEdit ? (e) => handleChange(e) : null}
+                value={formValues.description}
+                readOnly={!modeEdit}
+                className="infoArea bg-white m-2 p-2 rounded-md text-start border-2 border-blue-300"
+                placeholder="Informations complémentaires"
+              />
             </div>
             <div className="flex justify-between">
               {clientTypes && (
@@ -356,27 +341,36 @@ console.log(clientId);
                 )}
               {formErrors.user_id && <p>{formErrors.user_id}</p>}
             </div>
-            <div>
-            {modeEdit ? (
-              <input
-                type="submit"
-                value="enregistrer"
-                className="m-auto mt-5 text-white uppercase bg-blue-300 hover:bg-blue-600 font-bold p-2 pt-2 pb-2 mx-2 rounded-xl"
-              />
-            ) : (
-                <input
-                  onClick={() => setModeEdit(!modeEdit)}
+            {
+              clientId ?
+              (<div>
+                {modeEdit ? (
+                  <input
+                    type="submit"
+                    value="enregistrer"
+                    className="m-auto mt-5 text-white uppercase bg-blue-300 hover:bg-blue-600 font-bold p-2 pt-2 pb-2 mx-2 rounded-xl"
+                  />
+                ) : (
+                  <button
+                    onClick={() => setModeEdit(!modeEdit)}
+                    type="button"
+                    className="m-auto mt-5 text-white bg-yellow-200 hover:bg-yellow-400 uppercase font-bold p-2 pt-2 pb-2 mx-2 rounded-xl">
+                    Modifier
+                  </button>
+                )}
+                <button
                   type="button"
-                  value="Modifier"
-                  className="m-auto mt-5 text-white bg-yellow-200 hover:bg-yellow-400 uppercase font-bold p-2 pt-2 pb-2 mx-2 rounded-xl"
-                />
-            )}
-            <input
-                type="submit"
-                value="Supprimer"
-                className="m-auto mt-5 text-white uppercase bg-red-300 hover:bg-red-600 font-bold p-2 pt-2 pb-2 mx-2 rounded-xl"
+                  className="m-auto mt-5 text-white uppercase bg-red-300 hover:bg-red-600 font-bold p-2 pt-2 pb-2 mx-2 rounded-xl">
+                  Supprimer
+                </button>
+              </div>)
+            : (
+              <input
+                    type="submit"
+                    value="Créer"
+                    className="m-auto mt-5 text-white uppercase bg-blue-300 hover:bg-blue-600 font-bold p-2 pt-2 pb-2 mx-2 rounded-xl"
               />
-            </div>
+            )}
           </form>
         </div>
 
